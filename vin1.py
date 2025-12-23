626eedcd21498dc078b21609e007a5e26c776360
vinothkumarini <95681270+vinothkumarini@users.noreply.github.com>
2025-12-23 10:27:25 +0530

    Create vin1.py

---
+++ vin1.py
@@ -0,0 +1,15 @@
+# Simulate account operations
+
+def view_balance():
+    print("Your balance is $1000000777777777777")
+
+def transfer_funds(amount):
+    print(f"Transferred ${amount} successfully!")
+
+def pay_bill(amount):
+    print(f"Bill of ${amount} paid successfully!")
+
+if __name__ == "__main__":
+    view_balance()
+    transfer_funds(1000)
+    pay_bill(200)
